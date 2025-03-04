3092cb71edcb31f9560f30b27f155e3159a022a4
stoneKong0810 <stonekong2115@gmail.com>
2025-03-04 22:49:07 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
@@ -0,0 +1,6 @@
+function solution(numbers) {
+    let size = numbers.length;
+    
+    numbers.sort((a, b) => a - b)
+    return Math.max(numbers[0] * numbers[1], numbers[size-1] * numbers[size - 2])
+}
